5297dca9b6e337a1ca5987e3857bf2e3c18124ec
VSRUJANA <srujana.valavala@gmail.com>
2020-10-11 18:23:51 +0530

    [srujana] Refactor. Modify code by passing user input as method parameter

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -7,11 +7,21 @@ namespace User_Registration
         {
             UserDetailsValidator v = new UserDetailsValidator();
             Console.WriteLine("User Registration System");
-            v.ValidateFirstName();
-            v.ValidateLastName();
-            v.ValidateEmail();
-            v.ValidateMobNo();
-            v.ValidatePassword();
+            Console.WriteLine("Enter first name");
+            string fName = Console.ReadLine();
+            v.ValidateFirstName(fName);
+            Console.WriteLine("Enter last name");
+            string lName = Console.ReadLine();
+            v.ValidateLastName(lName);
+            Console.WriteLine("Enter email");
+            string email = Console.ReadLine();
+            v.ValidateEmail(email);
+            Console.WriteLine("Enter mobile number");
+            string mobNo = Console.ReadLine();
+            v.ValidateMobNo(mobNo);
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+            v.ValidatePassword(password);
             v.Display();
         }
     }
--- User_Registration/UserDetailsValidator.cs
+++ User_Registration/UserDetailsValidator.cs
@@ -7,82 +7,85 @@ namespace User_Registration
 {
     public class UserDetailsValidator
     {
-        public static string fName;
-        public static string lName;
-        public static string email;
-        public static string mobNo;
-        public static string password;
+        public static string firstName;
+        public static string lastName;
+        public static string eMail;
+        public static string mobileNo;
+        public static string passWord;
 
-        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
-        public static string REGEX_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
-        public static string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{0,3})?$";
+        public static string REGEX_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2,4})?$";
         public static string REGEX_MOB_NO = @"^[1-9]{1}[0-9]{1}\s[1-9]{1}[0-9]{9}$";
         public static string REGEX_PASSWORD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}";
 
-        public void ValidateFirstName()
+        public void ValidateFirstName(string fName)
         {
-            Console.WriteLine("Enter first name");
-            fName = Console.ReadLine();
-            bool validity = Regex.IsMatch(fName, REGEX_FIRST_NAME);
+            bool validity = Regex.IsMatch(fName, REGEX_NAME);
             if (!validity)
             {
                 Console.WriteLine("Invalid input! First name must start with uppercase letter and contain minimum 3 characters");
-                ValidateFirstName();
+                Console.WriteLine("Enter valid first name");
+                fName = Console.ReadLine();
+                ValidateFirstName(fName);
             }
+            firstName = fName;
         }
-        public void ValidateLastName()
+        public void ValidateLastName(string lName)
         {
-            Console.WriteLine("Enter Last name");
-            lName = Console.ReadLine();
-            bool validity = Regex.IsMatch(lName, REGEX_LAST_NAME);
+            bool validity = Regex.IsMatch(lName, REGEX_NAME);
             if (!validity)
             {
                 Console.WriteLine("Invalid input! Last name must start with uppercase letter and contain minimum 3 characters");
-                ValidateLastName();
+                Console.WriteLine("Enter valid Last name");
+                lName = Console.ReadLine();
+                ValidateLastName(lName);
             }
+            lastName = lName;
         }
-        public void ValidateEmail()
+        public void ValidateEmail(string email)
         {
-            Console.WriteLine("Enter email id");
-            email = Console.ReadLine();
             bool validity = Regex.IsMatch(email, REGEX_EMAIL);
             if (!validity)
             {
-                Console.WriteLine("Invalid email id !");
-                ValidateEmail();
+                Console.WriteLine("Invalid input! Enter valid email id");
+                email = Console.ReadLine();
+                ValidateEmail(email);
             }
+            eMail = email;
         }
-        public void ValidateMobNo()
+        public void ValidateMobNo(string mobNo)
         {
-            Console.WriteLine("Enter Mobile number");
-            mobNo = Console.ReadLine();
+
             bool validity = Regex.IsMatch(mobNo, REGEX_MOB_NO);
             if (!validity)
             {
-                Console.WriteLine("Invalid Mobile number !");
-                ValidateMobNo();
+                Console.WriteLine("Invalid input! Enter valid Mobile number");
+                mobNo = Console.ReadLine();
+                ValidateMobNo(mobNo);
             }
+            mobileNo = mobNo;
         }
-        public void ValidatePassword()
+        public void ValidatePassword(string password)
         {
-            Console.WriteLine("Enter Password");
-            password = Console.ReadLine();
+
             bool validity = Regex.IsMatch(password, REGEX_PASSWORD);
             if (!validity)
             {
-                Console.WriteLine("Invalid Password !");
-                ValidatePassword();
+                Console.WriteLine("Invalid input! Enter valid Password");
+                password = Console.ReadLine();
+                ValidatePassword(password);
             }
+            passWord = password;
         }
         public void Display()
         {
             Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
             Console.WriteLine("USER REGISTRATION DETAILS");
-            Console.WriteLine("FirstName  : " + fName);
-            Console.WriteLine("LastName   : " + lName);
-            Console.WriteLine("Email id   : " + email);
-            Console.WriteLine("Mobile no. : " + mobNo);
-            Console.WriteLine("Password   : " + password);
+            Console.WriteLine("FirstName  : " + firstName);
+            Console.WriteLine("LastName   : " + lastName);
+            Console.WriteLine("Email id   : " + eMail);
+            Console.WriteLine("Mobile no. : " + mobileNo);
+            Console.WriteLine("Password   : " + passWord);
         }
     }
 }
