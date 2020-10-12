f7ec136c57fe915c2b15a4aef9ae2c9e27dec514
VSRUJANA <srujana.valavala@gmail.com>
2020-10-12 19:46:35 +0530

    [srujana] Add. Parameterised test case to validate multiple entries of Email address added

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -125,5 +125,52 @@ namespace UserRegistrationTest
             //Assert
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void Validate_Multiple_Email_Enteries_for_valid_input(string email)
+        {
+            //Arrange
+            UserDetailsValidator validator = new UserDetailsValidator();
+            bool expected = true;
+            //Act
+            bool result = validator.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow("abc")]
+        [DataRow("abc@.com.my")]
+        [DataRow("abc123@gmail.a")]
+        [DataRow("abc123@.com")]
+        [DataRow("abc123@.com.com")]
+        [DataRow(".abc@abc.com")]
+        [DataRow("abc()*@gmail.com")]
+        [DataRow("abc@%*.com")]
+        [DataRow("abc..2002@gmail.com")]
+        [DataRow("abc.@gmail.com")]
+        [DataRow("abc@abc@gmail.com")]
+        [DataRow("abc@gmail.com.1a")]
+        [DataRow("abc@gmail.com.aa.au")]
+        public void Validate_Multiple_Email_Enteries_for_invalid_input(string email)
+        {
+            //Arrange
+            UserDetailsValidator validator = new UserDetailsValidator();
+            bool expected = false;
+            //Act
+            bool result = validator.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
     }
 }
